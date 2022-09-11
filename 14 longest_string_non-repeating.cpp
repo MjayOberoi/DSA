1a2a8f436974c765f55d7ae948cc708d1576139f
Mrityunjay Kumar <46307002+mrityunjay33@users.noreply.github.com>
2022-09-11 17:53:49 +0530

    3. Longest Substring Without Repeating Characters
    https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ 14 longest_string_non-repeating.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> um;
+        int i=0,count=0,mx=0;
+        while(i<s.length()){
+            if(um.count(s[i])== 0){
+                um[s[i]]=i;
+                count++;
+                mx = max(mx,count);
+            }
+            else{
+                int temp = i-um[s[i]];
+                if(temp<=count) count = temp;
+                else count++;
+                mx = max(mx,count);
+                um[s[i]]=i;
+            }
+            i++;
+        }
+        return mx;
+        
+    }
+};
