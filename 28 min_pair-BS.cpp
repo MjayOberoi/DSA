c65ac06db246a7531ee582aa58159d7551a48731
Mrityunjay Kumar <46307002+mrityunjay33@users.noreply.github.com>
2022-09-26 01:54:23 +0530

    Update 28 min_pair-BS.cpp

--- 28 min_pair-BS.cpp
+++ 28 min_pair-BS.cpp
@@ -5,8 +5,6 @@
 using namespace std;
 
 void min_pair(vector<int> a1,vector<int> a2){
-
-
 	//Sort the array
 	sort(a2.begin(),a2.end());
 
@@ -16,7 +14,6 @@ void min_pair(vector<int> a1,vector<int> a2){
 	//iteratte over 1 array and look for closes elements in the second array
 	for(int x : a1){
 		auto lb = lower_bound(a2.begin(),a2.end(),x) - a2.begin();
-
 		//left
 		if(lb>=1 and x - a2[lb-1] < diff){
 			diff = x - a2[lb-1];
@@ -29,20 +26,15 @@ void min_pair(vector<int> a1,vector<int> a2){
 			p1 = x;
 			p2 = a2[lb];
 		}
-
 	}
 	cout<<"Min Pair "<<p1<<" and "<<p2 <<endl;
 	return;
-
 }
 
-
 int  main(){
-
 	vector<int> a1 = {-1, 5, 10, 20, 3};
 	vector<int> a2 = {26, 134, 135, 15, 17};
 	min_pair(a1,a2);
-
 
 	return 0;
 }
