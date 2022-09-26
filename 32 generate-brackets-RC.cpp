c3d8a5385096f4886e0564c8d54173a1afb650ad
Mrityunjay Kumar <46307002+mrityunjay33@users.noreply.github.com>
2022-09-26 23:17:44 +0530

    Update 32 generate-brackets-RC.cpp

--- 32 generate-brackets-RC.cpp
+++ 32 generate-brackets-RC.cpp
@@ -7,18 +7,15 @@ void generateBrackets(string output,int n,int open,int close,int i){
 		cout<<output <<endl;
 		return;
 	}
-
-	//open  '(' 
-    //** opening should be less than input n
-	if(open<n){		
-        // output += '(';
-        // generateBrackets(output,n,open+1,close,i+1);
+ 
+    	// opening '('  should be less than input n
+	if(open<n){	
+        	// output += '('; generateBrackets(output,n,open+1,close,i+1); 
 		generateBrackets(output + '(',n,open+1,close,i+1);
-        // output.pop_back();
+        	// output.pop_back();
 	}
 
-	//closing  ')' 
-    // closing should always be less than or equal to opening. Hence, this addition of closing should happen only if it is less than opening
+    	// closing ')' should always be less than or equal to opening. Hence, this addition of closing should happen only if it is less than opening
 	if(close<open){
 		generateBrackets(output + ')',n,open,close+1,i+1);
 	}
