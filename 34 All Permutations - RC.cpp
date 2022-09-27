9515cdf1f2001f3cd7e5352007d2a56534548558
Mrityunjay Kumar <46307002+mrityunjay33@users.noreply.github.com>
2022-09-27 23:33:44 +0530

    Permutations : Leetcode
    https://leetcode.com/problems/permutations/

---
+++ 34 All Permutations - RC.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void helper(vector<vector<int>> &ans, vector<int> &nums, int index, int n){
+    
+    if(index==n){
+        ans.push_back(nums);
+        return;
+    }
+    
+    for(int i=index; i<=n; i++){
+        swap(nums[i],nums[index]);
+        helper(ans,nums,index+1,n);
+        swap(nums[i],nums[index]);
+    }
+}
+
+    
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> ans;
+    int n = nums.size()-1;
+
+    helper(ans,nums,0,n);
+
+    return ans;
+}
+};
