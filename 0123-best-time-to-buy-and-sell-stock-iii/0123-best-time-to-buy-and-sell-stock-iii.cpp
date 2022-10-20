8a63cc3660603c9b50565d38068008d477a46224
Mrityunjay Kumar <46307002+mrityunjay33@users.noreply.github.com>
2022-10-21 03:25:50 +0530

    Time: 1683 ms (7.30%), Space: 218.1 MB (9.55%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int helper(int index, int buy, int count, vector<int>& prices,int n, vector<vector<int>> &dp){
-        if(index==n || count < 0) return 0;
-        if(dp[index][buy] != -1) return dp[index][buy];
+    int helper(int index, int buy, int cap, vector<int>&prices, int n, vector<vector<vector<int>>>&dp){
+        if(index==n || cap == 0) return 0;
+        if(dp[index][buy][cap] != -1) return dp[index][buy][cap];
         int profit = 0;
-        if(buy)
-            profit = max(-prices[index]+helper(index+1,0,count,prices,n,dp),0+helper(index+1,1,count,prices,n,dp));
+        if(buy==1)
+            profit = max(-prices[index]+helper(index+1,0,cap,prices,n,dp),0+helper(index+1,1,cap,prices,n,dp));
         else
-            profit = max(prices[index]+helper(index+1,1,count-1,prices,n,dp),0+helper(index+1,0,count,prices,n,dp));
-        return dp[index][buy]=profit;
+            profit = max(prices[index]+helper(index+1,1,cap-1,prices,n,dp),0+helper(index+1,0,cap,prices,n,dp));
+        return dp[index][buy][cap]=profit;
     }
     
     
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
        
+        /*
+         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
         for(int index=n-1;index>=0;index--){
             for(int buy=1;buy>=0;buy--){
                 for(int cap=1; cap<=2;cap++){
@@ -28,5 +30,8 @@ public:
             }
         }        
         return dp[0][1][2];
+        */
+        
+        return helper(0,1,2,prices,n,dp);
     }
 };
