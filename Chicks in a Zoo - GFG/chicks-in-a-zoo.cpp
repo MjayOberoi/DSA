cbb7b84997e1f9e0fc2a648a95bf8543233135b2
Mrityunjay Kumar <46307002+mrityunjay33@users.noreply.github.com>
2022-10-30 17:00:13 +0530

    Added solution - LeetHub

---
+++ Chicks in a Zoo - GFG/chicks-in-a-zoo.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	long long int NoOfChicks(int n){
+	    long long int dp[n+1];
+        dp[1] = 1;
+        long long int chicks = 1;
+        for(int i=2; i<=n; i++){
+            if(i>6) chicks = chicks-dp[i-6];
+            dp[i] = 2*chicks;
+            chicks = chicks + 2*chicks;
+        }
+        return chicks;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int N;
+		cin >> N;
+		Solution obj;
+		long long int ans = obj.NoOfChicks(N);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
