94b63ab24ac91e82d83274cf5e0b7da459c87867
Mrityunjay Kumar <46307002+mrityunjay33@users.noreply.github.com>
2022-12-11 14:07:37 +0530

    Time: 66 ms (18.42%), Space: 27.6 MB (65.92%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -21,13 +21,14 @@ public:
         int l = max(0,subTreesum(root->left,mx));
         int r = max(0,subTreesum(root->right,mx));
 
-        int curr = root->val+l+r;                             // curr node + left node + right node (path sum for curr node)
-        cout<<curr<<" ";
+        int curr = root->val+l+r;   // curr node + left node + right node (path sum for curr node)
+      //  cout<<curr<<" ";
 
         mx = max(mx,curr);                                    // took max out of all
 
-        return root->val+max(l,r);                            // max(l,r) means we will chose one path that will gave max path sum to us
+        return root->val+max(l,r);     // max(l,r) means we will chose one path that will gave max path sum to us
     }
+    
     int maxPathSum(TreeNode* root) 
     {
         int mx = INT_MIN;
